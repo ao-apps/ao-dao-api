539e5b832200a15795717d93b737d29509ff2b99
AO Industries, Inc <support@aoindustries.com>
2021-06-29 02:54:17 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,30 @@
+/*
+ * ao-dao-api - Simple data access objects framework API.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-dao-api.
+ *
+ * ao-dao-api is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-dao-api is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-dao-api.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.aoapps.dao.api {
+	exports com.aoapps.dao;
+	// Direct
+	requires com.aoapps.dbc; // <groupId>com.aoapps</groupId><artifactId>ao-dbc</artifactId>
+	requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
+	// Java SE
+	requires java.sql;
+}
